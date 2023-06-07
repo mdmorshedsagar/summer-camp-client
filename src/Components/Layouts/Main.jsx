4c0819255397ccb515c86659aa5cfb42bde86f70
mdmorshedsagar <mdmosahidsagar5304257@gmail.com>
2023-06-07 10:28:43 -1200

    simple-routes-create

---
+++ src/Components/Layouts/Main.jsx
@@ -0,0 +1,11 @@
+
+
+const Main = () => {
+    return (
+        <div>
+            dsfsdfds
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Components/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            fgdffhg
+        </div>
+    );
+};
+
+export default Home;
