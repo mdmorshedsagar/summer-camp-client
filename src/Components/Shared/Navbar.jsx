cc4b27e22c8a1a434e20a98683c2bcf1c4fb9fc0
mdmorshedsagar <mdmosahidsagar5304257@gmail.com>
2023-06-08 08:34:19 -1200

    modify-navbar

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProviders";
+import logo from "../../assets/logo.webp";
 const Navbar = () => {
   const {user,  logOut } = useContext(AuthContext);
   
@@ -40,88 +41,103 @@ const Navbar = () => {
   </>
 
   return (
-    <div>
-      <nav className="bg-white shadow">
-        <div className="container mx-auto px-4">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex items-center">
-              <Link to="/" className="text-lg font-semibold">
-                Your Website
-              </Link>
-            </div>
-            <div className="hidden md:flex">
-              <div className="ml-10 flex items-center  space-x-4">
-                <Link to="/" className="text-gray-600 hover:text-gray-900">
-                  Home
-                </Link>
-                <Link to="/register" className="text-gray-600 hover:text-gray-900">
-                  Registration
-                </Link>
-                <Link
-                  to="/services"
-                  className="text-gray-600 hover:text-gray-900"
-                >
-                  Services
-                </Link>
-                <Link
-                  to="/contact"
-                  className="text-gray-600 hover:text-gray-900"
-                >
-                  Contact
-                </Link>
-           {navItems}
-              </div>
-            </div>
-            <div className="md:hidden flex">
-              <button className="mobile-menu-button" onClick={toggleMobileMenu}>
-                <svg
-                  className="h-6 w-6 text-gray-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              </button>
-            </div>
+    <div className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
+    <nav className="container mx-auto max-w-screen-xl px-4 md:py-4">
+      <div className="flex items-center justify-between h-16">
+        <div className="flex items-center">
+          <Link to="/">
+            <img
+              className="w-[180px] h-auto"
+              src={logo}
+              alt="Logo"
+            />
+          </Link>
+        </div>
+        <div className="hidden md:flex">
+          <div className="ml-10 flex items-center space-x-4">
+            <Link
+              to="/"
+              className="text-gray-600 hover:text-gray-900"
+            >
+              Home
+            </Link>
+            <Link
+              to="/register"
+              className="text-gray-600 hover:text-gray-900"
+            >
+              Registration
+            </Link>
+            <Link
+              to="/services"
+              className="text-gray-600 hover:text-gray-900"
+            >
+              Services
+            </Link>
+            <Link
+              to="/contact"
+              className="text-gray-600 hover:text-gray-900"
+            >
+              Contact
+            </Link>
+            {navItems}
           </div>
         </div>
-        {isMobileMenuOpen && (
-          <div className="md:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              <Link to="/" className="text-gray-600 hover:text-gray-900 block">
-                Home
-              </Link>
-              <Link
-                to="/about"
-                className="text-gray-600 hover:text-gray-900 block"
-              >
-                About
-              </Link>
-              <Link
-                to="/services"
-                className="text-gray-600 hover:text-gray-900 block"
-              >
-                Services
-              </Link>
-              <Link
-                to="/contact"
-                className="text-gray-600 hover:text-gray-900 block"
-              >
-                Contact
-              </Link>
-           {navItems}
-            </div>
-          </div>
-        )}
-      </nav>
+        <div className="md:hidden flex">
+          <button
+            className="mobile-menu-button"
+            onClick={toggleMobileMenu}
+          >
+            <svg
+              className="h-6 w-6 text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </nav>
+  {isMobileMenuOpen && (
+    <div className="md:hidden">
+      <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+        <Link
+          to="/"
+          className="text-gray-600 hover:text-gray-900 block"
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className="text-gray-600 hover:text-gray-900 block"
+        >
+          About
+        </Link>
+        <Link
+          to="/services"
+          className="text-gray-600 hover:text-gray-900 block"
+        >
+          Services
+        </Link>
+        <Link
+          to="/contact"
+          className="text-gray-600 hover:text-gray-900 block"
+        >
+          Contact
+        </Link>
+        {navItems}
+      </div>
     </div>
+  )}
+</div>
+
   );
 };
 
