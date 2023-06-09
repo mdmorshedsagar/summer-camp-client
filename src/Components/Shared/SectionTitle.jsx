5ac1da4ac8cba36397ec5722053296115f826c44
mdmorshedsagar <mdmosahidsagar5304257@gmail.com>
2023-06-08 13:20:49 -1200

    create-section-component

---
+++ src/Components/Shared/SectionTitle.jsx
@@ -0,0 +1,10 @@
+export const SectionTitle =({heading, subHeading}) =>{
+    <div className="mx-auto text-center md:w-4/12 my-8">
+    <h3 className="text-yellow-600 mb-2">{heading}</h3>
+
+    <p className="text-3xl uppercase border-y-4 py-4">{subHeading}</p>
+    <hr/>
+</div>
+}
+
+
